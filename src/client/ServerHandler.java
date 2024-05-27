8e2501d2fd91a475d0fbed9c84c9f95e151a5bf8
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-05-27 12:05:47 +0200

    added code for serverhandler

--- src/client/ServerHandler.java
+++ src/client/ServerHandler.java
@@ -1,4 +1,70 @@
 package client;
 
+import data.Workout;
+import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ServerHandler {
+
+    public ServerHandler instance = new ServerHandler();
+
+    private Socket socket;
+    private ExecutorService pool = Executors.newFixedThreadPool(1);
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    private ObservableList<Workout> workouts;
+
+    private ServerHandler() {
+    }
+
+    public void connect() throws IOException {
+        if (socket != null && socket.isConnected()) {
+            startListening();
+            return;
+        }
+
+        socket = new Socket("localhost", 8000); //todo
+        input = new ObjectInputStream(socket.getInputStream());
+        output = new ObjectOutputStream(socket.getOutputStream());
+        startListening();
+    }
+
+    private void startListening() {
+        if (pool.isTerminated()) {
+            pool.execute(() -> {
+                try {
+                    workouts = (ObservableList<Workout>) input.readObject();
+                    while (socket.isConnected()) {
+                        workouts.add((Workout) input.readObject());
+                    }
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+    public void disconnect() throws IOException {
+        if (socket != null) socket.close();
+        pool.shutdown();
+    }
+
+    public void uploadWorkout(Workout workout) throws IOException {
+        if (socket == null || !socket.isConnected()) {
+            return;
+        }
+
+        output.writeObject(workout);
+        output.flush();
+    }
+
+    public ObservableList<Workout> getServerWorkouts() {
+        return workouts;
+    }
 }
