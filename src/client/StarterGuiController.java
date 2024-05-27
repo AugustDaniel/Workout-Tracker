eeec7b46e6ec25d680c7083596179e37c9113048
Pieter <pieter.degraaff@icloud.com>
2024-05-27 14:09:14 +0200

    Logica toegevoegd start, en refreshbutton toegevoegd

--- src/client/StarterGuiController.java
+++ src/client/StarterGuiController.java
@@ -1,16 +1,24 @@
 package client;
 
+import data.Exercise;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class StarterGuiController {
     @FXML
     private Button workoutstarter_back_button;
+    @FXML
+    private ListView<Exercise> workoutstarter_exercises_list;
     public void handleBackButton(ActionEvent actionEvent) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
@@ -25,9 +33,16 @@ public class StarterGuiController {
     }
 
     public void handleAddButton(ActionEvent actionEvent) {
+        if (workoutstarter_exercises_list.getSelectionModel().getSelectedItems()!=null){
+//            workoutstarter_exercises_list.getSelectionModel().getSelectedItems().get(0).addSet();
+        }
     }
 
     public void handleFinishButton(ActionEvent actionEvent) {
         //todo
+    }
+
+    public void initialize() {
+        workoutstarter_exercises_list.getItems().setAll(Client.getActualWorkout().getExcercises());
     }
 }
