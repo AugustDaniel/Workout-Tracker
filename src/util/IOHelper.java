c0f1498a0adc53f48b03bb3ea96bc38b5eca8f35
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-05-28 18:51:50 +0200

    added IOHelper

---
+++ src/util/IOHelper.java
@@ -0,0 +1,29 @@
+package util;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public final class IOHelper {
+
+    public static <T> void saveObject(T object, File file) throws Exception {
+        ObjectOutputStream output = new ObjectOutputStream(Files.newOutputStream(file.toPath()));
+        output.writeObject(object);
+        output.close();
+    }
+
+    public static Object readObject(File file) throws Exception {
+        ObjectInputStream input = new ObjectInputStream(Files.newInputStream(file.toPath()));
+        Object object = input.readObject();
+        input.close();
+        return object;
+    }
+
+    public static File getFileFromChooser(String message) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(message);
+        return fileChooser.showOpenDialog(new Stage());
+    }
+}
